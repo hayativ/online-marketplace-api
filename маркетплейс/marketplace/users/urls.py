a7915a47bd6f16f0a3859a80c4336e957735ba01
ainshine <ainfromparadise@gmail.com>
2025-10-12 09:38:36 +0500

    moved products and users to apps, deleted marketplace

--- маркетплейс/marketplace/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, UserMeView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('me/', UserMeView.as_view(), name='me'),
-]
