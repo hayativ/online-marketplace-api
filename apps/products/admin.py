3967ca7459750c00e882df47c8796ef3542187fb
hayativ <davydova.04.eva@mail.ru>
2025-10-14 00:25:50 +0500

    admin panel configured, records data added

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,13 +1,80 @@
 from django.contrib import admin
-
-from .models import Product, Category
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    pass
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "name",
+        "description",
+    )
+    search_fields = ("name",)
+    list_filter = (
+        "name",
+    )
+    fieldsets = [
+        (
+            "Category Information",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                ),
+            },
+        ),
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "category",
+        "seller",
+        "price",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "category__name",
+        "seller__email",
+    )
+    list_filter = (
+        "category",
+        "seller",
+        "created_at",
+    )
+    ordering = ("-created_at",)
+
+    fieldsets = [
+        (
+            "Product Information",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "price",
+                    "image",
+                ),
+            },
+        ),
+        (
+            "Relations",
+            {
+                "fields": (
+                    "category",
+                    "seller",
+                ),
+            },
+        ),
+        (
+            "Date Information",
+            {
+                "fields": ("created_at",),
+            },
+        ),
+    ]
+
+    readonly_fields = ("created_at",)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,85 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+    )
+    search_fields = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+    )
+    list_filter = (
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+    )
+    ordering = ("-date_joined",)
+
+    fieldsets = [
+        (
+            "User Information",
+            {
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+            },
+        ),
+    ]
+
+    add_fieldsets = [
+        (
+            "Create User",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    ]
+
+    readonly_fields = ("last_login", "date_joined")
